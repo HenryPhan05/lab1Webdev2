0197482d16f7130335cafc08335c1ddaebccfa8e
Thanh Vinh <henry.phan2601@gmail.com>
2026-01-20 15:30:07 -0700

    Update page.tsx

--- app/week-2/page.tsx
+++ app/week-2/page.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import StudentInfo from "./student-info";
 export default function Page() {
   return (
     <>
       <main>
         <div>
+          <h1>Week 2 Assignment</h1>
           <h1>Shopping List </h1>
           <StudentInfo></StudentInfo>
+          <br />
+          <br />
+          <br />
+          <Link href="/" >Home page</Link>
         </div>
       </main >
     </>
